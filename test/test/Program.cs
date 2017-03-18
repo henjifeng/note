8acc368159f351abf34d9e858ad93eff40962380
zengwei <leizhihen@leizhihen-PC>
2017-03-18 17:28:40 +0800

    test

---
+++ test/test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("这是一个测试程序  运行在vs2013  期望能运行在vs2017上");
+            Console.ReadKey();
+        }
+    }
+}
